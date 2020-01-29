a8d5a044df755a4e4e61854d90832a346a89e953
Vasanthi Lingamaneni <vlingamaneni@HOAR.com>
2020-01-29 13:48:39 -0600

    1)Added NonInventoryItem to Print  page. 2)Fixed the issue with editing non stock items. 3)Removed NonInventory items as Hidden field which throwed error. 4)Added LocationId instead of Location to set the default stock location for users. 5)In AccountingInvoice.html and Invoice.html canged the url to printsummary instead of InvoiceSummary.

---
+++ WIS/WIS/Models/LocationViewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WIS.Models
+{
+    public class LocationViewmodel
+    {
+        public IEnumerable<SelectListItem> locationlist { get; set; }
+        public int LocationID { get; set; }
+        public string LocationDescription { get; set; }
+    }
+}
